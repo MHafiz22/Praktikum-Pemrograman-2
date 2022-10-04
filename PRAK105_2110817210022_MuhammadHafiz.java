47f6788dc808bd7856aa87e95d821e51b9edd9ee
MHafiz22 <114972345+MHafiz22@users.noreply.github.com>
2022-10-04 17:08:34 +0800

    Add files via upload

---
+++ PRAK105_2110817210022_MuhammadHafiz.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prak105_2110817210022_muhammadhafiz;
+
+import java.util.Scanner;
+import java.text.DecimalFormat;
+
+/**
+ *
+ * @author USER
+ */
+public class PRAK105_2110817210022_MuhammadHafiz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        float jarijari, tinggi;
+        double volume;
+        Scanner keyboard = new Scanner(System.in);
+        
+        System.out.print("Masukan jari-jari : ");
+        jarijari = keyboard.nextFloat();
+        
+        System.out.print("Masukan tinggi    : ");
+        tinggi = keyboard.nextFloat();
+        
+        volume = 3.14 * jarijari * jarijari * tinggi;
+        DecimalFormat df = new DecimalFormat("#.###");
+        
+        System.out.println("Volume tabung dengan jari-jari " + jarijari + " cm dan tinggi " + tinggi + " cm adalah " + df.format(volume) + " m3");
+    }
+}
