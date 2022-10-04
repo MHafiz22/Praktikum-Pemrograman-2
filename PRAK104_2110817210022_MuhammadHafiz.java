afeb731be43fbcb044e6fed64fbadd35fc4cf92a
MHafiz22 <114972345+MHafiz22@users.noreply.github.com>
2022-10-04 15:52:04 +0800

    Add files via upload

---
+++ PRAK104_2110817210022_MuhammadHafiz.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package prak104_2110817210022_muhammadhafiz;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author USER
+ */
+public class PRAK104_2110817210022_MuhammadHafiz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int andi = 0, budi = 0;
+        
+        Scanner keyboard = new Scanner(System.in);
+        
+        System.out.println("Kartu Andi : ");
+        int angkaandi1 = keyboard.nextInt();
+        int angkaandi2 = keyboard.nextInt();
+        int angkaandi3 = keyboard.nextInt();
+        
+        System.out.println("Kartu Budi : ");
+        int angkabudi1 = keyboard.nextInt();
+        int angkabudi2 = keyboard.nextInt();
+        int angkabudi3 = keyboard.nextInt();
+                
+        if(angkaandi1 > angkabudi1){
+            andi++;
+        }else if(angkaandi1 == angkabudi1){
+            
+        }else{
+            budi++;
+        }
+        
+        if(angkaandi2 > angkabudi2){
+            andi++;
+        }else if(angkaandi2 == angkabudi2){
+            
+        }else{
+            budi++;
+        }
+        
+        if(angkaandi3 > angkabudi3){
+            andi++;
+        }else if(angkaandi3 == angkabudi3){
+            
+        }else{
+            budi++;
+        }
+        
+        if(andi > budi){
+            System.out.println("Andi");
+        }else if(andi < budi) {
+            System.out.println("Budi");
+        }else {
+            System.out.println("Seri");
+        }
+    }
+}
